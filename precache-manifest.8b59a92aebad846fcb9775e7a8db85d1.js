1394143f0c69acd68f60c2c741396aee3708027a
rowan91 <rowanprofessor@yahoo.com>
2019-08-01 07:24:31 -0500

    initial commit

---
+++ precache-manifest.8b59a92aebad846fcb9775e7a8db85d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92959e2e76608dc5fb5f5b712d10672e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96c21abc249f14a089ce",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "e8b483718874a95ad7ef",
+    "url": "/static/css/main.68d28862.chunk.css"
+  },
+  {
+    "revision": "96c21abc249f14a089ce",
+    "url": "/static/js/2.92afcf6d.chunk.js"
+  },
+  {
+    "revision": "e8b483718874a95ad7ef",
+    "url": "/static/js/main.ea10f77c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
