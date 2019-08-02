9c09fd7efe05b8650644823e2a259ea9fd0aa024
rowan91 <rowanprofessor@yahoo.com>
2019-08-02 17:40:07 -0500

    fixing...

---
+++ precache-manifest.edc70d25b924e9e9aa8f8b0b3ebb0abf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abb72df8179bae766a7a26f8363f914",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96c21abc249f14a089ce",
+    "url": "/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "745d1e3a1b02b1c2eece",
+    "url": "/static/css/main.68d28862.chunk.css"
+  },
+  {
+    "revision": "96c21abc249f14a089ce",
+    "url": "/static/js/2.92afcf6d.chunk.js"
+  },
+  {
+    "revision": "745d1e3a1b02b1c2eece",
+    "url": "/static/js/main.34fb6d42.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
