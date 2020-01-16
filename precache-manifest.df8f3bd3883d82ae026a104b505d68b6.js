3678ee9660c548802f08659ba4f39d2a211814e3
rowan91 <rowanprofessor@yahoo.com>
2020-01-16 14:36:13 -0600

    adding Pichu...

--- precache-manifest.df8f3bd3883d82ae026a104b505d68b6.js
+++ precache-manifest.df8f3bd3883d82ae026a104b505d68b6.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "fa7b47bda1bf80e6e7410fec1f8930a0",
+    "revision": "12e1685468f320d7c24ef77a205228b0",
     "url": "/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.ae9e8b99.chunk.css"
   },
   {
-    "revision": "9b28a193fb1941fccb10",
-    "url": "/static/css/main.68d28862.chunk.css"
+    "revision": "5f1197cbd7edf64268c6",
+    "url": "/static/css/main.9ebdf66f.chunk.css"
   },
   {
     "revision": "96c21abc249f14a089ce",
     "url": "/static/js/2.92afcf6d.chunk.js"
   },
   {
-    "revision": "9b28a193fb1941fccb10",
-    "url": "/static/js/main.28d5c03a.chunk.js"
+    "revision": "5f1197cbd7edf64268c6",
+    "url": "/static/js/main.bcb89b6a.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
